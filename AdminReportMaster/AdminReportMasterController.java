0ced08a5b5f0726ae70d64ffcbb0670f184459f3
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2026-01-04 00:26:37 +0530

    Enhance AdminReportMasterController with new repositories
    Added repositories for ReviewMaster and UserMaster to the AdminReportMasterController. Updated ShowReports method to include counts and lists for reports, reviews, and users.

--- AdminReportMaster/AdminReportMasterController.java
+++ AdminReportMaster/AdminReportMasterController.java
@@ -3,7 +3,12 @@ package com.example.kaisi_lagi.AdminReportMaster;
 //import ch.qos.logback.core.model.Model;
 import com.example.kaisi_lagi.ReportMaster.ReportMaster;
 import com.example.kaisi_lagi.ReportMaster.ReportRepository;
+import com.example.kaisi_lagi.ReviewMaster.ReviewMaster;
+import com.example.kaisi_lagi.ReviewMaster.ReviewRepository;
+import com.example.kaisi_lagi.UserMaster.UserMaster;
+import com.example.kaisi_lagi.UserMaster.UserRepository;
 import jakarta.persistence.GeneratedValue;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,31 +24,26 @@ public class AdminReportMasterController {
     AdminReportMasterRepository adminReportMasterRepository;
     @Autowired
     ReportRepository reportRepository;
+    @Autowired
+    ReviewRepository reviewRepository;
+    @Autowired
+    UserRepository userRepository;
 
-//    @GetMapping("/ShowReports")
-//    public String ShowReports(Model model ){
-//        Iterable<AdminReportMaster>reports=adminReportMasterRepository.findAll();
-//        List<AdminReportMaster>listreports=new ArrayList<>();
-//        reports.forEach(listreports::add);
-//        model.addAttribute("showAllReports", listreports);
-//        return "AdminReportMasterForm";
-//
-//    }
 
     @GetMapping("/ShowReports")
-    public String ShowReports(Model model ){
-        Iterable<ReportMaster>reports=reportRepository.findAll();
-
-        List<ReportMaster>listreports=new ArrayList<>();
-        reports.forEach(listreports::add);
-        model.addAttribute("showAllReports", listreports);
+    public String ShowReports(Model model){
+        Long reportMaster=reportRepository.count();
+        Long reviewMaster=reviewRepository.count();
+        Long userMaster=userRepository.count();
+        List<ReportMaster>reportList=reportRepository.findAll();
+        List<ReviewMaster>reviewList=reviewRepository.findAll();
+        List<UserMaster>userList=userRepository.findAll();
+        model.addAttribute("reportMaster",reportMaster);
+        model.addAttribute("reviewMaster",reviewMaster);
+        model.addAttribute("userMaster",userMaster);
+        model.addAttribute("reportList",reportList);
+        model.addAttribute("reviewList",reviewList);
+        model.addAttribute("userList",userList);
         return "AdminReportMasterForm";
-
-    }
-
-    @GetMapping("/delete/{admin_id}")
-    public String deleteReport(@PathVariable long admin_id){
-        adminReportMasterRepository.deleteById(admin_id);
-        return "redirect:/ShowReports";
     }
 }
